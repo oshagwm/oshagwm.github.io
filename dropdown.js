1a4c38051ca70560ff02c8ff972f0d83bbaa549f
oshagwm <williammichaeloshaugh@gmail.com>
2025-05-21 19:36:38 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -2,12 +2,3 @@ function toggleDropdown() {
   const dropdown = document.getElementById('dropdownContent');
   dropdown.classList.toggle('show');
 }
-
-// Optional: Close dropdown if clicking outside
-window.addEventListener('click', function (e) {
-  const button = document.querySelector('.dropbtn');
-  const dropdown = document.getElementById('dropdownContent');
-  if (!button.contains(e.target) && !dropdown.contains(e.target)) {
-    dropdown.classList.remove('show');
-  }
-});
